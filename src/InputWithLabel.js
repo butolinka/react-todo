380ee73eb89e70663c426872a1ab6b1aa468402f
Mariia Butolina <butolinkam@gmail.com>
2023-11-25 19:31:48 -0500

    Added autofocus on input

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function InputWithLabel ({todoTitle, handlerTitleChange, children}){
-
+    const inputRef = React.useRef();
+    React.useEffect(() =>{
+            inputRef.current.focus();
+    });
     return(
         <>
             <label htmlFor='todoTitle'>{children} </label>
             <input 
+            ref={inputRef}
             type="text" 
             id='todoTitle' 
             name='title' 
