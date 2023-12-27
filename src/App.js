db0b64c7cf6900d3c5b3f3f7f91bcea9ea0d5444
Mariia Butolina <butolinkam@gmail.com>
2023-12-27 17:48:28 -0500

    POST method moved to APP component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import AddToDoForm from './AddTodoForm';
 function App() {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsloading] = React.useState(true);
-  const url = `https://api.airtable.com/v0/
-  ${process.env.REACT_APP_AIRTABLE_BASE_ID}/
-  ${process.env.REACT_APP_TABLE_NAME}`;
-  console.log(url);
+  const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
   const fetchData = async() => {
     const options = {
       method: 'GET',
@@ -16,7 +13,6 @@ function App() {
         Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
         
       },
-      
     };
     try{
     const response = await fetch(url, options);
@@ -33,14 +29,40 @@ function App() {
       console.log(error.message);
     }
   };
-
-  React.useEffect(() =>{
+    React.useEffect(() =>{
     fetchData();
+     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const addTodo = (newTodo) =>{
-    setTodoList((previousTodoList) => [...previousTodoList, newTodo]);
+  const addNewTodo = async(title) =>{
+    const addTitle = {
+      fields: {
+        title: title.title,
+      },
+    };
+    console.log('title:', title);
+    
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+    },
+    body: JSON.stringify(addTitle),
   };
+  try{
+    const response = await fetch(url, requestOptions);
+    if (!response.ok){
+      throw new Error(`Error:${response.status}`);
+    }
+    const todo = await response.json();
+    const newTodo = {id: todo.id, title: todo.fields.title};
+    setTodoList([...todoList, newTodo]);
+  } catch(error){
+    console.log(error.message);
+    return null;
+  }
+};
+
   function removeTodo(id){
     const todoUpgrade = todoList.filter((todo)=> id !== todo.id);
     setTodoList(todoUpgrade);
@@ -48,7 +70,7 @@ function App() {
   return (
     <>
     <h1>Todo List </h1>
-      <AddToDoForm onAddTodo={addTodo} />
+      <AddToDoForm onAddTodo={addNewTodo} />
     {isLoading? (<p>Loading...</p> ):
       (<ToDoList todoList={todoList} onRemoveTodo={removeTodo}/>
       )}
